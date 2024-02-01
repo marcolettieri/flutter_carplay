643b4edefee08257e3377f51f02815d7840d30cd
Marco Lettieri <m.lettieri@microbees.com>
2024-02-01 16:22:41 +0100

    applied @vanlooverenkoen fix to use the carplay app when iphone app is killed

--- ios/Classes/FlutterCarplayPluginSceneDelegate.swift
+++ ios/Classes/FlutterCarplayPluginSceneDelegate.swift
@@ -62,10 +62,10 @@ class FlutterCarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelega
     SwiftFlutterCarplayPlugin.onCarplayConnectionChange(status: FCPConnectionTypes.connected)
     let rootTemplate = SwiftFlutterCarplayPlugin.rootTemplate
 
-    guard rootTemplate != nil else {
-      FlutterCarPlaySceneDelegate.interfaceController = nil
-      return
+    if rootTemplate != nil {
+      FlutterCarPlaySceneDelegate.interfaceController?.setRootTemplate(rootTemplate!, animated: SwiftFlutterCarplayPlugin.animated, completion: nil)
     }
+
     
     FlutterCarPlaySceneDelegate.interfaceController?.setRootTemplate(rootTemplate!, animated: SwiftFlutterCarplayPlugin.animated)
   }
